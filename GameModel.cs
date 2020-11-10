31d199e3c46f81d9144a209aeec4eb671ac7338e
erdelevsky <zh.vibe18@yandex.ru>
2020-11-10 17:42:08 +0500

    field creation and scoring

---
+++ GameModel.cs
@@ -0,0 +1,11 @@
+﻿namespace LittleTetris
+{
+    
+    public static class GameModel
+    { 
+        public static readonly bool[,] field = new bool[Constants.CELL_COUNT_X, Constants.CELL_COUNT_Y];
+        public static Figure figure = new Figure();
+        public static int gameScore = 0;
+        public static int destroyedLines = 0;
+    }
+}
---
+++ LineChecker.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace LittleTetris
+{
+    public static class LineChecker
+    {
+        public static void IsTooHigh()
+        {
+            for (int i = 0; i < Constants.CELL_COUNT_X; i++)
+                if (GameModel.field[i, 3])
+                    Environment.Exit(0);
+        }
+
+        public static void FindFilledLines()
+        {
+            List<int> filledLines = new List<int>(4);
+            bool lineIsFilled;
+            for (int i = 0; i < Constants.CELL_COUNT_Y; i++)
+            {
+                lineIsFilled = true;
+                for (int j = 1; j < Constants.CELL_COUNT_X; j++)
+                {
+                    if (!GameModel.field[j, i])
+                    {
+                        lineIsFilled = false;
+                        break;
+                    }
+                }
+                if (lineIsFilled)
+                    filledLines.Add(i);
+            }
+            DestroyFilledLines(filledLines);
+        }
+
+        private static void DestroyFilledLines(List<int> filledLines)
+        {
+            if (filledLines.Count == 0)
+                return;
+            int multiple = 1;
+            foreach (int line in filledLines)
+            {
+                for (int i = line; i > 0; i--)
+                    for (int j = 1; j < Constants.CELL_COUNT_X; j++)
+                        GameModel.field[j, i] = GameModel.field[j, i - 1];
+                GameModel.gameScore += Constants.SCORE_FOR_LINE * multiple;
+                GameModel.destroyedLines++;
+                multiple++;
+            }
+
+        }
+    }
+}
